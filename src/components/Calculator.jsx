4efaccec6f7ef3abd3d3d20de6d76b1a4ba5e194
ArpitGupta_0908 <2300033545@kluniversity.in>
2025-03-05 21:26:16 +0530

    Updated Theme Changes

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -89,28 +89,95 @@ const Calculator = () => {
       document.body.style.color = "white";
       document.body.style.boxShadow = "0px 0px 20px rgba(255, 255, 255, 0.5)";
       document.body.style.transition = "all 0.5s ease-in-out";
+
       let calElement = document.getElementsByClassName("cal")[0];
       if (calElement) {
         calElement.style.color = "blue";
         calElement.style.backgroundColor = "blue";
+        calElement.style.borderColor="white";
       }
+
       let backelement = document.getElementsByClassName("digits")[0];
       if (backelement) {
-        backelement.style.backgroundColor = "rgba(240, 86, 39, 0.81)";
+        backelement.style.backgroundColor = "rgba(39, 240, 196, 0.81)";
+      }
+
+      let keys = document.getElementsByClassName("btn");
+      for (let i = 0; i < keys.length; i++) {
+        keys[i].style.backgroundColor = "rgb(0, 0, 0)";
+        keys[i].style.color = "white";
+        keys[i].style.transition = "all 0.5s ease-in-out";
+      }
+
+      let keyssymbols = document.getElementsByClassName("btn-sy");
+      for (let j = 0; j < keyssymbols.length; j++) {
+        keyssymbols[j].style.backgroundColor = "rgb(255, 153, 0)";
+        keyssymbols[j].style.color = "black";
+        keyssymbols[j].style.transition = "all 0.5s ease-in-out";
+      }
+      let btnclear = document.getElementsByClassName("btn-clear");
+      for (let j = 0; j < btnclear.length; j++) {
+        btnclear[j].style.backgroundColor = "rgb(200, 0, 0)";
+      }
+      let btneq = document.getElementsByClassName("btneq")[0];
+      if (btneq) {
+        btneq.style.backgroundColor = "green";
+      }
+      let rootElement = document.querySelector("#root");
+      if (rootElement) {
+        rootElement.style.backgroundColor = "rgb(100, 201, 38)";
+      }
+      let heading = document.querySelector("h1");
+      if (heading) {
+        heading.style.color = "white";
       }
     } else {
       document.body.style.backgroundColor = "white";
       document.body.style.color = "black";
       document.body.style.boxShadow = "none";
       document.body.style.transition = "all 0.5s ease-in-out";
+
       let calElement = document.getElementsByClassName("cal")[0];
       if (calElement) {
         calElement.style.color = "red";
-        calElement.style.backgroundColor = "yellowgreen";
+        calElement.style.backgroundColor = "rgb(71, 201, 38)";
+        calElement.style.borderColor="black";
       }
+
       let backelement = document.getElementsByClassName("digits")[0];
       if (backelement) {
         backelement.style.backgroundColor = "lightblue";
+      }
+
+      let keys = document.getElementsByClassName("btn");
+      for (let i = 0; i < keys.length; i++) {
+        keys[i].style.backgroundColor = "rgb(255, 255, 255)";
+        keys[i].style.color = "black";
+        keys[i].style.transition = "all 0.5s ease-in-out";
+      }
+
+      let keyssymbols = document.getElementsByClassName("btn-sy");
+      for (let j = 0; j < keyssymbols.length; j++) {
+        keyssymbols[j].style.backgroundColor = "yellow";
+        keyssymbols[j].style.color = "black";
+        keyssymbols[j].style.transition = "all 0.5s ease-in-out";
+      }
+      let btnclear = document.getElementsByClassName("btn-clear");
+      for (let j = 0; j < btnclear.length; j++) {
+        btnclear[j].style.backgroundColor = "rgb(255,100,100)";
+      }
+      let btneq = document.getElementsByClassName("btneq")[0];
+      if (btneq) {
+        btneq.style.backgroundColor = "rgb(0, 255, 119)";
+      }
+
+      let rootElement = document.querySelector("#root");
+      if (rootElement) {
+        rootElement.style.backgroundColor = "rgb(243, 158, 54)";
+      }
+      let heading = document.querySelector("h1");
+      if (heading) {
+        heading.style.color = "rgb(20, 236, 185)";
       }
     }
   }
